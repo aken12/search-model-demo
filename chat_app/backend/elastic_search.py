64d97db6142e5b0156b44b7eb38ae0c0c824682e
aken12 <94615661+aken12@users.noreply.github.com>
2024-03-03 03:37:19 +0900

    Revert "two-model"

--- chat_app/backend/elastic_search.py
+++ chat_app/backend/elastic_search.py
@@ -28,72 +28,40 @@ es = Elasticsearch(
 )
 
 ## 要修正
-def search_documents(index,query,model,model_names):
+def search_documents(index,query,model,model_name):
     results = {"result1": {} ,"result2": {"result": None}}
 
-    print(type(model),len(model))
+    model_dict = {"DPR": dense_search,"Hyblid":hyblid_search}
 
-    model_dict = {
-        "No":bm25_search,
-        "BM25": bm25_search,
-        "E5-large": lambda index, query: dense_search(index, query, model=model[0], large=True),
-        "E5-small": lambda index, query: dense_search(index, query, model=model[1], large=False),
-        "Hyblid": lambda index, query: hybrid_search(index, query, model=model[0]),
-    }
+    if model_name[1] == "No":
+        if model_name[0] == "model1" or model_name[0] == "BM25":
+            results["result1"]["result"],results["result1"]["search_time"] = bm25_search(index, query)
+            results["result1"]["name"] = "BM25"
+            return results
+        else:
+            results["result1"]["result"],results["result1"]["search_time"] = model_dict[model_name[0]](index, query, model)
+            results["result1"]["name"] = model_name[0]
+            return results
+        
+    if model_name[0] == "BM25":
+        results["result1"]["result"],results["result1"]["search_time"] = bm25_search(index, query)
+        results["result1"]["name"] = "BM25"
+    elif  model_name[0] in model_dict.keys():
+        results["result1"]["result"],results["result1"]["search_time"] = model_dict[model_name[0]](index, query, model)
+        results["result1"]["name"] = model_name[0]
 
-    for i,model_name in enumerate(model_names):
-        i += 1
-        if model_name in model_dict:
-            search_result, search_time = model_dict[model_name](index,query)
-            results[f'result{i}'] = {
-                "name": model_name,
-                "result": search_result,
-                "search_time": search_time
-            }
-    # print(results)
-    print(len(results))
-    print(results.keys())
-    print(results["result1"].keys())
+    if model_name[1] == "BM25":
+        results["result2"] = {}
+        results["result2"]["result"],results["result2"]["search_time"] = bm25_search(index, query)
+        results["result2"]["name"] = "BM25"
+    elif  model_name[1] in model_dict.keys():
+        results["result2"] = {}
+        results["result2"]["result"],results["result2"]["search_time"] = model_dict[model_name[1]](index, query, model)
+        results["result2"]["name"] = model_name[1]
+
     return results
 
-    # model_dict = {
-    #     "BM25": bm25_search,
-    #     "ベクトル検索（E5-large）": lambda idx, qry: dense_search(idx, qry, model),
-    #     "ベクトル検索（E5-small）": lambda idx, qry: dense_search(idx, qry, model),
-    #     "Hybrid": lambda idx, qry: hybrid_search(idx, qry, model)
-    # }
-
-
-    # model_dict = {"DPR": dense_search,"Hyblid":hyblid_search}
-    # if model_name[1] == "No":
-    #     if model_name[0] == "model1" or model_name[0] == "BM25":
-    #         results["result1"]["result"],results["result1"]["search_time"] = bm25_search(index, query)
-    #         results["result1"]["name"] = "BM25"
-    #         return results
-    #     else:
-    #         results["result1"]["result"],results["result1"]["search_time"] = model_dict[model_name[0]](index, query, model)
-    #         results["result1"]["name"] = model_name[0]
-    #         return results
-        
-    # if model_name[0] == "BM25":
-    #     results["result1"]["result"],results["result1"]["search_time"] = bm25_search(index, query)
-    #     results["result1"]["name"] = "BM25"
-    # elif  model_name[0] in model_dict.keys():
-    #     results["result1"]["result"],results["result1"]["search_time"] = model_dict[model_name[0]](index, query, model)
-    #     results["result1"]["name"] = model_name[0]
-
-    # if model_name[1] == "BM25":
-    #     results["result2"] = {}
-    #     results["result2"]["result"],results["result2"]["search_time"] = bm25_search(index, query)
-    #     results["result2"]["name"] = "BM25"
-    # elif  model_name[1] in model_dict.keys():
-    #     results["result2"] = {}
-    #     results["result2"]["result"],results["result2"]["search_time"] = model_dict[model_name[1]](index, query, model)
-    #     results["result2"]["name"] = model_name[1]
-
-    # return results
-
-def bm25_search(index,query,model=None):
+def bm25_search(index,query):
     body={
         "size": 10,
         'query':{
@@ -113,22 +81,17 @@ def bm25_search(index,query,model=None):
     end = time.time() 
 
     search_time = end-start
-    # print(res)
+    print(res)
     return res['hits']['hits'],search_time
 
-def dense_search(index,query,model,large=False):
+def dense_search(index,query,model):
     with torch.no_grad():
-        encoded_query = model.generate_e5_embs("query: " + query)
-
-    if large:
-        field = "doc_vector_large" 
-    else:
-        field = "doc_vector"
+        encoded_query = model.generate_e5_embs(query)
 
     es_query = {
         "size": 100,
         "knn": {
-            "field": field,
+            "field": "doc_vector",
             "query_vector": encoded_query.tolist()[0],
             "k":100,
             "num_candidates":1000
@@ -141,17 +104,15 @@ def dense_search(index,query,model,large=False):
     search_time = end-start
     return res['hits']['hits'], search_time
 
-def hybrid_search(index,query,model):
+def hyblid_search(index,query,model):
     with torch.no_grad():
-        encoded_query = model.generate_e5_embs("query: " + query)
-
-    print("ここはOK")
+        encoded_query = model.generate_e5_embs(query)
 
     es_query = {
                 "size": 100,
                 "query": {"multi_match": {"query": query, "fields": ["subject_name", "course_overview"]}},
                 "knn": {
-                        "field": "doc_vector_large",
+                        "field": "doc_vector",
                         "query_vector": encoded_query.tolist()[0],
                         "k": 100,
                         "num_candidates": 1000,
@@ -161,7 +122,7 @@ def hybrid_search(index,query,model):
     start = time.time()
     res = es.search(index=index,body=es_query)
     end = time.time()
-    # print(res)
+    print(res)
     search_time = end-start
     print(search_time)
     return res['hits']['hits'], search_time
